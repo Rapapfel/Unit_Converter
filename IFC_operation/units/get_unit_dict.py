6eb2b251663dab854c3ffea01fa3706b4ee99c84
Rapapfel <raphael.gremaud@stud.hslu.ch>
2023-12-15 18:57:20 +0100

    vsfasdf

--- IFC_operation/units/get_unit_dict.py
+++ IFC_operation/units/get_unit_dict.py
@@ -1,3 +1,4 @@
+import sympy
 from sympy import Symbol, simplify
 from sympy.physics import units
 from sympy.physics.units.dimensions import Dimension
@@ -15,12 +16,16 @@ def replace_symbols_in_formula(formula, replacements):
 # Sammeln aller verfügbaren Einheiten
 all_units = {name: getattr(units, name) for name in dir(units) if isinstance(getattr(units, name), units.Unit)}
 
+# Volumen und Zeit für Volumenstrom auslesen
+volume = filter_units_by_dimension(all_units, Dimension("length")**3)
+time_units = filter_units_by_dimension(all_units, Dimension("time"))
+
 # Formeln für verschiedene Kategorien
 formulas = {
     "Fläche": ["length**2"],
     "Volumen": ["length**3"],
     "Dichte": ["mass / (length**3)"],
-    "Volumenstrom": ["(length**3) / time"],
+    "Volumenstrom": ["(length**3) / time", "volume / time"],
     "Massenstrom": ["mass / time"],
     "Druck": ["force / (length**2)"],
     "Geschwindigkeit": ["length / time"],
@@ -61,18 +66,30 @@ def create_units_dictionary():
     }
 
     # Erweitern der Einheiten durch Formeln
+    # Erweitern der Einheiten durch Formeln
     for category in units_dict:
         if category in formulas:
             for formula in formulas[category]:
-                expr = simplify(formula)
-                symbols = expr.free_symbols
-                dimensions_needed = [str(symbol) for symbol in symbols]
-                units_combinations = product(*(filter_units_by_dimension(all_units, Dimension(dim)) for dim in dimensions_needed))
+                if 'volume' in formula:
+                    # Verwenden Sie die zuvor definierten Volumeneinheiten und iterieren über Zeit-Einheiten
+                    for vol_unit in volume:
+                        vol_unit_str = str(vol_unit)
+                        for time_unit in time_units:
+                            time_unit_str = str(time_unit)
+                            evaluated_formula = formula.replace('volume', vol_unit_str).replace('time', time_unit_str)
+                            evaluated_expr = simplify(evaluated_formula)
+                            units_dict[category].add(str(evaluated_expr))
+                else:
+                    expr = simplify(formula)
+                    symbols = expr.free_symbols
+                    dimensions_needed = [str(symbol) for symbol in symbols]
+                    units_combinations = product(*(filter_units_by_dimension(all_units, Dimension(dim)) for dim in dimensions_needed))
 
-                for combo in units_combinations:
-                    replacements = dict(zip(dimensions_needed, combo))
-                    evaluated_formula = replace_symbols_in_formula(expr, replacements)
-                    units_dict[category].add(str(evaluated_formula))
+                    for combo in units_combinations:
+                        replacements = dict(zip(dimensions_needed, combo))
+                        evaluated_formula = replace_symbols_in_formula(expr, replacements)
+                        units_dict[category].add(str(evaluated_formula))
+
 
     return units_dict
 
@@ -81,9 +98,9 @@ units_dictionary = create_units_dictionary()
 
 def convert_symbols_to_strings(obj):
     """
-    Rekursive Funktion, um Symbol-Objekte in Strings umzuwandeln
+    Rekursive Funktion, um SymPy-Objekte in Strings umzuwandeln
     """
-    if isinstance(obj, Symbol):
+    if isinstance(obj, (Symbol, sympy.Basic)):  # Inkludiert Relational-Objekte
         return str(obj)
     elif isinstance(obj, set):
         return {convert_symbols_to_strings(item) for item in obj}
@@ -94,25 +111,25 @@ def convert_symbols_to_strings(obj):
 
 def convert_sets_to_lists(obj):
     """
-    Rekursive Funktion, um Set-Objekte in Listen umzuwandeln
+    Rekursive Funktion, um Set-Objekte in sortierte Listen umzuwandeln
     """
     if isinstance(obj, set):
-        return [convert_sets_to_lists(item) for item in obj]
+        return sorted([convert_sets_to_lists(item) for item in obj])
     elif isinstance(obj, dict):
         return {k: convert_sets_to_lists(v) for k, v in obj.items()}
     else:
         return obj
+
+# Pfad der Datei, in die das JSON gespeichert werden soll
+json_file_path = r'C:\Users\ragre\OneDrive - Hochschule Luzern\DC_Scripting\unit_changer\IFC_operation\units\units.json'
 
 # Erstellen des Dictionaries
 units_dictionary = create_units_dictionary()
 
 # Anwenden der Konvertierungsfunktionen
 units_dictionary_converted = convert_symbols_to_strings(units_dictionary)
-units_dictionary_lists = convert_sets_to_lists(units_dictionary_converted)
+units_dictionary_lists_sorted = convert_sets_to_lists(units_dictionary_converted)
 
-# Pfad der Datei, in die das JSON gespeichert werden soll
-json_file_path = r'C:\Users\ragre\OneDrive - Hochschule Luzern\DC_Scripting\unit_changer\IFC_operation\units\units.json'
-
-# Schreiben des JSON-Strings in die Datei
+# Schreiben des sortierten JSON-Strings in die Datei
 with open(json_file_path, 'w') as file:
-    json.dump(units_dictionary_lists, file, indent=4)
+    json.dump(units_dictionary_lists_sorted, file, indent=4)
